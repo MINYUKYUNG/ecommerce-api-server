41d7fa7057e42385548ab4a9c3b630604d12f0e7
MINYUKYUNG <minew0223@gmail.com>
2022-12-18 16:44:04 +0900

    Feature: CORS 추가

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.query);
