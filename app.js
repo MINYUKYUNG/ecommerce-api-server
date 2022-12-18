bb204c22fcb8753f4b8c38dc8a125006522073cb
MINYUKYUNG <minew0223@gmail.com>
2022-12-18 21:20:42 +0900

    Fix: DB 이미지 불러오기 실패 -> url 주소 변경

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
