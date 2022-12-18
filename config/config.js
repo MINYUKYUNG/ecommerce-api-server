a7e618cede677789a684529632bf3e28bbe32886
MINYUKYUNG <minew0223@gmail.com>
2022-12-18 19:35:29 +0900

    Modify: DB host 주소 추가

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
@@ -16,8 +16,8 @@ module.exports = {
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB,
-    "host": "127.0.0.1",
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
